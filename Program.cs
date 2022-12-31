eb734ac1716d7be841040cb00b67724af468ff7f
Paul Anderson <paul.robert.anderson@gmail.com>
2022-12-31 15:30:23 +1100

    Initial commit of Dump1090 Data Replay program

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ReplayDump1090Data
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Configuration
+            const int SpeedFactor = 10; //1 for realtime, 10 for 10x faster etc
+            const int TCPPort = 30003; // The port to listen for incoming connections on
+            string filePath = "dump1090.txt"; // The path to the file with the data to replay
+
+            // Create a new TCP listener
+            TcpListener listener = new TcpListener(IPAddress.Any, TCPPort);
+
+            // Start the listener
+            listener.Start();
+            Console.WriteLine("Listening for incoming connections on port {0}...", TCPPort);
+
+            // Wait for a client to connect
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+
+            // Open the file with the data to replay
+            StreamReader reader = new StreamReader(filePath);
+
+            // Read the data from the file one line at a time
+            string line = reader.ReadLine();
+            DateTime lastTimestamp = DateTime.MinValue;
+            while (line != null)
+            {
+                // Extract the timestamp from the line
+                Match match = Regex.Match(line, @"^.+,(\d+/\d+/\d+,\d+:\d+:\d+\.\d+),.+$");
+                if (match.Success)
+                {
+                    DateTime timestamp = DateTime.Parse(match.Groups[1].Value);
+
+                    // Calculate the delay between this line and the last one
+                    if (lastTimestamp != DateTime.MinValue)
+                    {
+                        TimeSpan delay = timestamp - lastTimestamp;
+                        if (SpeedFactor!=1) delay/=SpeedFactor;
+                        Console.WriteLine("Delaying for {0} seconds before sending next line...", delay.TotalSeconds);
+                        Thread.Sleep(delay);
+                    }
+
+                    lastTimestamp = timestamp;
+                }
+
+                // Send the line to the client
+                byte[] buffer = Encoding.ASCII.GetBytes(line);
+                client.GetStream().Write(buffer, 0, buffer.Length);
+
+                // Read the next line from the file
+                line = reader.ReadLine();
+            }
+
+            // Close the file and the client connection
+            reader.Close();
+            client.Close();
+
+            // Stop the listener
+            listener.Stop();
+
+            Console.WriteLine("Replay finished. Last line sent at {0}.", lastTimestamp);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Dump1090-Data-Replay
-This program replays data from a file in the format produced by the Dump1090 ADS-B decoder software. It listens for incoming connections on a specified port, then reads the data from the file one line at a time and sends it to the client.  The timestamp is extracted from each line in the file and used to time the output of each line to the client.
+# Dump1090 Data Replay
+
+This program replays data from a file in the format produced by the Dump1090 ADS-B decoder software. It listens for incoming connections on a specified port, then reads the data from the file one line at a time and sends it to the client. The program extracts the timestamp from each line in the file and calculates the delay between it and the previous line, then sends the line to the client and repeats the process until it reaches the end of the file.
+
+**Note:** The data for this program can be captured by running [Dump1090ConsolePlaneTracker](https://github.com/PaulAnderson/Dump1090ConsolePlaneTracker) with logging enabled.
+
+## Usage
+
+1. Install the .NET Core runtime, if it is not already installed on your system.
+2. Clone or download this repository.
+3. Open a command prompt in the repository directory.
+4. Run the following command:
+
+dotnet run
+
+Copy code
+
+## Configuration
+
+The following configuration options can be modified in the `Program.cs` file:
+
+- `TCPPort`: The port to listen for incoming connections on.
+- `filePath`: The path to the file with the data to replay.
